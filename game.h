be910a0ee81a81a56fa8a8f8730ce323da346e77
Alexandre Lemyre <alemyre@z3r7p2.42lyon.fr>
2026-04-25 15:16:22 +0200

    going left logic + reset cell merged

--- game.h
+++ game.h
@@ -6,19 +6,40 @@
 #include <ncurses.h>
 
 
+enum e_const
+{
+    WIN_VALUE = 2048
+};
+
+typedef struct s_grid
+{
+    int     row;
+    int     column;
+    int     score;
+    t_cell  cell_grid[4][4];
+}           t_grid;
+
+typedef struct s_cell
+{
+    int     value;
+    int     merged;
+}           t_cell;
+
+
 typedef struct s_game_data
 {
-    int game_running;
-    int nb_of_cell;
-    int key;
-    int cell_height;
-    int cell_width;
-    int board_height;
-    int board_width;
-    int margin;
-    int start_y;
-    int start_x;
-} t_game_data;
+    int     game_running;
+    int     nb_of_cell;
+    int     key;
+    int     cell_height;
+    int     cell_width;
+    int     board_height;
+    int     board_width;
+    int     margin;
+    int     start_y;
+    int     start_x;
+    t_grid  grid;
+}           t_game_data;
 
 
 void    draw_board(t_game_data *g_data);
@@ -27,5 +48,7 @@ void    key_hook(t_game_data *g_data);
 int     ncurses_print_nbr(int nb, int x, int y);
 int     count_len_number(int nb);
 int     get_center_offset(int container_size, int item_size);
+void    reset_merged(t_grid *grid);
+void    going_left(t_grid *grid);
 
 #endif
---
+++ utils/swipe.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swipe.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alemyre <alemyre@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 14:57:25 by alemyre           #+#    #+#             */
+/*   Updated: 2026/04/25 15:10:47 by alemyre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.h"
+
+int    going_left(t_grid *grid)
+{
+    int moved = 0;
+    int y = 0;
+    
+    while (y < grid->row)
+    {
+        int x = 1;
+        while (x < grid->column)
+        {
+            int index = 0;
+            if (grid->cell_grid[x][y].value != 0)
+            {
+                while (grid->cell_grid[x - index - 1][y].value == 0)
+                {
+                    grid->cell_grid[x - index - 1][y].value += grid->cell_grid[x - index][y].value;    
+                    grid->cell_grid[x - index][y].value = 0;
+                    moved++;
+                    index++;
+                }
+                if ((grid->cell_grid[x - index - 1][y].value == grid->cell_grid[x - index][y].value)
+                && grid->cell_grid[x - index - 1][y].merged == 0)
+                {
+                    grid->cell_grid[x - index - 1][y].value += grid->cell_grid[x - index][y].value;
+                    grid->cell_grid[x - index][y].value = 0;
+                }
+                x++;
+            }
+        }
+        y++;
+    }
+    return (moved);
+}
